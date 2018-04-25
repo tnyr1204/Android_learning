a9138a83cc181c68ec0c09615f6d649cbb676cd7
tnyr1204 <tani.yuri.tu3@is.naist.jp>
2018-04-25 14:17:46 +0900

    じゃんけんアプリ(画面遷移のみ)

---
+++ Janken_ex/app/src/main/java/com/example/uni/janken_ex/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.uni.janken_ex;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onJankenButtonTapped(View view) {
+        Intent intent = new Intent(this, ResultActivity.class);
+        startActivity(intent);
+    }
+}
