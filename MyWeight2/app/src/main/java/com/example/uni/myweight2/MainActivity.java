25a74fc6ab272294e075adea7fd3543074cf58bb
tnyr1204 <tani.yuri.tu3@is.naist.jp>
2018-05-09 15:21:25 +0900

    保存ダイアログ追加

--- MyWeight2/app/src/main/java/com/example/uni/myweight2/MainActivity.java
+++ MyWeight2/app/src/main/java/com/example/uni/myweight2/MainActivity.java
@@ -1,9 +1,35 @@
 package com.example.uni.myweight2;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static class MainDialogFragment extends DialogFragment {
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+            builder.setTitle("保存処理");
+            builder.setMessage("保存します");
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    //保存処理
+                }
+            });
+            return builder.create();
+        }
+    }
+
+    public void onSaveButtonTapped(View view) {
+        DialogFragment newFragment = new MainDialogFragment();
+        newFragment.show(getFragmentManager(), "MainFragmentDialog");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
