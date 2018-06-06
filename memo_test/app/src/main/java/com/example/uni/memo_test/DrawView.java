25edd54ef029cd54572404705e10884f2d1ed1a6
tnyr1204 <tani.yuri.tu3@is.naist.jp>
2018-06-06 13:25:31 +0900

    お絵かきアプリ Undo・Redo追加

--- memo_test/app/src/main/java/com/example/uni/memo_test/DrawView.java
+++ memo_test/app/src/main/java/com/example/uni/memo_test/DrawView.java
@@ -5,23 +5,21 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.graphics.PorterDuff;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnTouchListener;
 
-import java.util.ArrayList;
+import java.util.Stack;
 
 public class DrawView extends View implements OnTouchListener{
-
 	private Paint paint;
-
-	private ArrayList<Path> pathList = new ArrayList<Path>();
+	private final Stack<Path> undoStack = new Stack<Path>();
+	private final Stack<Path> redoStack = new Stack<Path>();
 	private Path path;
-
-	public DrawView(Context context) {
-		super(context);
-	}
+	private Canvas canvas;
 
 	public DrawView(Context context, AttributeSet attrs) {
 		super(context, attrs);
@@ -31,13 +29,15 @@ public class DrawView extends View implements OnTouchListener{
 		paint.setColor(Color.BLACK);
 		paint.setStyle(Paint.Style.STROKE);
 		paint.setAntiAlias(true);
-		paint.setStrokeWidth(10);
+		paint.setStrokeWidth(5);
+		paint.setStrokeCap(Paint.Cap.ROUND);
+		canvas = new Canvas();
 	}
 
 	@Override
 	protected void onDraw(Canvas canvas) {
 		super.onDraw(canvas);
-		for (Path path : pathList) {
+		for (Path path : undoStack) {
 			canvas.drawPath(path, paint);
 		}
 	}
@@ -49,8 +49,9 @@ public class DrawView extends View implements OnTouchListener{
 			case MotionEvent.ACTION_DOWN:
 				// タッチしたとき
 				path = new Path();
-				path.moveTo(event.getX(), event.getY());
-				pathList.add(path);
+				path.moveTo(event.getX()-1, event.getY()-1);
+				undoStack.push(path);
+				redoStack.clear();
 				break;
 			case MotionEvent.ACTION_MOVE:
 				// タッチしたまま動かしたとき
@@ -66,4 +67,30 @@ public class DrawView extends View implements OnTouchListener{
 		return true;
 	}
 
+	public Path undo() {
+		if(!undoStack.isEmpty()) {
+			path = undoStack.pop();
+			redoStack.push(path);
+			invalidate();
+		}
+
+		return path;
+	}
+
+	public Path redo() {
+		if(!redoStack.isEmpty()) {
+			path = redoStack.pop();
+			undoStack.push(path);
+			invalidate();
+		}
+
+		return path;
+	}
+
+	public void clear() {
+		Log.d("DrawView", "クリア");
+		canvas.drawColor(0, PorterDuff.Mode.CLEAR);
+		redoStack.clear();
+		invalidate();
+	}
 }
--- memo_test/app/src/main/java/com/example/uni/memo_test/MainActivity.java
+++ memo_test/app/src/main/java/com/example/uni/memo_test/MainActivity.java
@@ -1,16 +1,54 @@
 package com.example.uni.memo_test;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
 
-public class MainActivity extends Activity {
-
+public class MainActivity extends AppCompatActivity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
-		DrawView view = new DrawView(this);
 		setContentView(R.layout.activity_main);
+
+		//元に戻す
+		ImageButton undo = (ImageButton) findViewById(R.id.undo);
+		undo.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				DrawView drawView = (DrawView)findViewById(R.id.draw1);
+				drawView.undo();
+			}
+		});
+
+		//やり直す
+		ImageButton redo = (ImageButton) findViewById(R.id.redo);
+		redo.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				DrawView drawView = (DrawView)findViewById(R.id.draw1);
+				drawView.redo();
+			}
+		});
+
+		//消去
+		ImageButton clear = (ImageButton) findViewById(R.id.clear);
+		clear.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				DrawView drawView = (DrawView)findViewById(R.id.draw1);
+				drawView.clear();
+			}
+		});
 	}
 
+	//ビューサイズ確認
+	@Override
+	public void onWindowFocusChanged(boolean hasFocus) {
+		super.onWindowFocusChanged(hasFocus);
+		Log.d("MainActivity", "横幅: " + findViewById(R.id.draw1).getWidth());
+		Log.d("MainActivity", "縦幅: " + findViewById(R.id.draw1).getHeight());
+	}
 }
